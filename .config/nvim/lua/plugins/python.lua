1e7688688355f4e694ba628dfec41fe8fd356e57
Dave Leslie <david@kickup.co>
2025-07-08 12:00:10 -0400

    Add stepout for dap

--- .config/nvim/lua/plugins/python.lua
+++ .config/nvim/lua/plugins/python.lua
@@ -139,6 +139,7 @@ return {
       vim.keymap.set("n", "<F5>", dap.continue)
       vim.keymap.set("n", "<F10>", dap.step_over)
       vim.keymap.set("n", "<F11>", dap.step_into)
+      vim.keymap.set("n", "<c+F11>", dap.step_out)
       vim.keymap.set("n", "<F12>", dap.step_out)
       vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
       vim.keymap.set("n", "<Leader>dB", function()

c04ad7f977e24ff1ad8d7e75dc6f71a6837daeb7
Dave Leslie <david@kickup.co>
2025-10-21 16:24:13 -0400

    Add spectre for find/replace

---
+++ .config/nvim/lua/plugins/spectre.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "nvim-pack/nvim-spectre",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>sr", "<cmd>Spectre<CR>", desc = "Open Spectre" },
+    },
+  },
+}

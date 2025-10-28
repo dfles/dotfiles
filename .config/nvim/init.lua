1128308abfd35189f0ee8147f385173ec8104329
Dave Leslie <david@kickup.co>
2025-10-28 12:53:50 -0400

    no-neck-pain.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -86,6 +86,18 @@ require("lazy").setup({
       indent = { enable = true },
     },
   },
+  -- Zen(ish) mode with note taking in the gutters
+  {
+    "shortcuts/no-neck-pain.nvim",
+    version = "*",
+    opts = {
+      width = 150,
+    },
+    keys = {
+      { "<leader>z", "<cmd>NoNeckPain<cr>", desc = "Toggle centered window" },
+    },
+  },
+  -- Additional, more involved plugin config
   { import = "plugins" },
 })
 

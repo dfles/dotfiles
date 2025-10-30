e400b7a629aee8e925664734163e719d394e796a
Dave Leslie <david@kickup.co>
2025-10-30 11:54:29 -0400

    Disable arrow keys in i/n/v

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,9 +1,11 @@
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic quickfix list" })
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic" })
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+-- Surely this will stop me from using arrow keys
+for _, mode in ipairs({ "n", "v", "i" }) do
+  vim.keymap.set(mode, "<Up>", "<Nop>", { noremap = true, silent = true })
+  vim.keymap.set(mode, "<Down>", "<Nop>", { noremap = true, silent = true })
+  vim.keymap.set(mode, "<Left>", "<Nop>", { noremap = true, silent = true })
+  vim.keymap.set(mode, "<Right>", "<Nop>", { noremap = true, silent = true })
+end
 
--- Buffers
 vim.keymap.set("n", "<leader>bo", "<cmd>%bd|e#<CR>", { desc = "Close other buffers" })
 -- Delete current buffer without closing window
 vim.keymap.set("n", "<leader>bd", "<cmd>bp<bar>sp<bar>bn<bar>bd<CR>", { desc = "Delete buffer" })

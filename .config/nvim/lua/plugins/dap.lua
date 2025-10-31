bdd989b5b585e49232ed80fe562b92cda6abf334
Dave Leslie <david@kickup.co>
2025-10-31 13:24:07 -0400

    Move base dap configuration to its own file

---
+++ .config/nvim/lua/plugins/dap.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "nvim-neotest/nvim-nio",
+    },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+
+      dapui.setup()
+      -- Gracefully handle not having a python virtual env active.
+      -- In general this debugging should be re-thought so we can support multiple debuggers.
+      vim.keymap.set("n", "<F5>", dap.continue)
+      vim.keymap.set("n", "<F10>", dap.step_over)
+      vim.keymap.set("n", "<F11>", dap.step_into)
+      vim.keymap.set("n", "<F12>", dap.step_out)
+      vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
+      vim.keymap.set("n", "<Leader>dB", function()
+        dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+      end, { desc = "Conditional breakpoint" })
+      vim.keymap.set("n", "<Leader>dr", dap.restart, { desc = "Restart" })
+
+      vim.keymap.set("n", "<Leader>dt", dap.terminate, { desc = "Stop debugging" })
+
+      -- Auto open/close UI
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+  },
+}
--- .config/nvim/lua/plugins/python.lua
+++ .config/nvim/lua/plugins/python.lua
@@ -89,48 +89,15 @@ local debug_configs = {
 
 return {
   {
-    "mfussenegger/nvim-dap",
+    "mfussenegger/nvim-dap-python",
     dependencies = {
-      "rcarriga/nvim-dap-ui",
-      "mfussenegger/nvim-dap-python",
-      "nvim-neotest/nvim-nio",
+      "mfussenegger/nvim-dap",
     },
     config = function()
       local dap = require("dap")
-      local dapui = require("dapui")
 
-      dapui.setup()
-      -- Gracefully handle not having a python virtual env active.
-      -- In general this debugging should be re-thought so we can support multiple debuggers.
-      local pycmd = "python3"
-      if os.getenv("VIRTUAL_ENV") then
-        pycmd = os.getenv("VIRTUAL_ENV") .. "/bin/python"
-      end
-
-      require("dap-python").setup(pycmd)
+      require("dap-python").setup()
       dap.configurations.python = debug_configs
-      vim.keymap.set("n", "<F5>", dap.continue)
-      vim.keymap.set("n", "<F10>", dap.step_over)
-      vim.keymap.set("n", "<F11>", dap.step_into)
-      vim.keymap.set("n", "<F12>", dap.step_out)
-      vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
-      vim.keymap.set("n", "<Leader>dB", function()
-        dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
-      end, { desc = "Conditional breakpoint" })
-      vim.keymap.set("n", "<Leader>dr", dap.restart, { desc = "Restart" })
-
-      vim.keymap.set("n", "<Leader>dt", dap.terminate, { desc = "Stop debugging" })
-
-      -- Auto open/close UI
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited["dapui_config"] = function()
-        dapui.close()
-      end
     end,
   },
 }

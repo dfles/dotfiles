5bca8666379963d5a0591f22686fb3275465e6c2
Dave Leslie <david@kickup.co>
2025-11-07 13:28:38 -0500

    More basedpyright tweaks

--- .config/nvim/lsp/basedpyright.lua
+++ .config/nvim/lsp/basedpyright.lua
@@ -10,11 +10,19 @@ return {
   single_file_support = true,
   handlers = {
     ["textDocument/publishDiagnostics"] = function() end,
+    ["window/logMessage"] = function(err, result, ctx, config)
+      -- Suppress enumeration warnings
+      if result and result.message and result.message:match("Enumeration of workspace") then
+        return
+      end
+      -- Call default handler for other messages
+      vim.lsp.handlers["window/logMessage"](err, result, ctx, config)
+    end,
   },
   settings = {
     basedpyright = {
       analysis = {
-        autoSearchPaths = true,
+        autoSearchPaths = false, -- Disable workspace scanning
         useLibraryCodeForTypes = true,
         diagnosticMode = "openFilesOnly",
         extraPaths = { "./backend" },
@@ -30,8 +38,20 @@ return {
           "**/.venv",
           "**/venv",
           "**/.git",
+          "./frontend",
+          "./node_modules",
+          "./.ruff_cache",
+          "./.mypy_cache",
+          "./.pytest_cache",
+          "./dist",
+          "./build",
+          "./__pycache__",
+          "./.venv",
+          "./venv",
+          "./.git",
         },
-        ignore = { "*" }, -- Only analyze open files
+        ignore = { "*" },
+        indexing = false, -- Disable background indexing
       },
     },
   },

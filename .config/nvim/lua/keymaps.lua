cdb3ca91f35090c71ee7721935cafa600b5663b9
Dave Leslie <david@kickup.co>
2026-02-24 16:35:22 -0500

    LSP checkhealth and make full blame a toggle

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -14,6 +14,7 @@ vim.keymap.set("n", "<leader>ba", "<cmd>%bd<cr>", { desc = "Delete all buffers" 
 vim.keymap.set("n", "<leader>bo", "<cmd>%bd|e#<cr>", { desc = "Delete other buffers" })
 -- Delete current buffer without closing window
 vim.keymap.set("n", "<leader>bd", "<cmd>bp<bar>sp<bar>bn<bar>bd<cr>", { desc = "Delete current buffer" })
+vim.keymap.set("n", "<leader>bl", "<cmd>checkhealth lsp<cr>", { desc = "LSP info" })
 
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -39,7 +39,20 @@ return {
     },
     keys = {
       { "<leader>gi", "<cmd>Gitsigns toggle_current_line_blame<CR>", desc = "Toggle blame" },
-      { "<leader>gI", "<cmd>Gitsigns blame<CR>", desc = "Full blame" },
+      {
+        "<leader>gI",
+        function()
+          for _, win in ipairs(vim.api.nvim_list_wins()) do
+            local buf = vim.api.nvim_win_get_buf(win)
+            if vim.api.nvim_get_option_value("filetype", { buf = buf }) == "gitsigns-blame" then
+              vim.api.nvim_win_close(win, false)
+              return
+            end
+          end
+          require("gitsigns").blame()
+        end,
+        desc = "Toggle full blame",
+      },
     },
   },
 }
